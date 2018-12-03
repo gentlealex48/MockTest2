7614f4c02299e5dfa8b2f7ffafe4630a70e642a9
alexander ebigwei <gentlealex48@yahoo.com>
2018-12-03 17:51:27 -0500

    Initial commit

---
+++ app/src/main/java/com/kkaty/mocktest2/Room.java
@@ -0,0 +1,10 @@
+package com.kkaty.mocktest2;
+
+public class Room {
+    public final boolean isInfected;
+    public boolean visited = false;
+    Room(boolean infected){
+        isInfected = infected;
+    }
+
+}
